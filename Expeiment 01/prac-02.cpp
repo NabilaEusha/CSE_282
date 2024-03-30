19aad95d0b9716239cab9771c56da367ce9551b4
Nabila Sultana Eusha <86979752+NabilaEusha@users.noreply.github.com>
2024-03-30 18:40:40 +0600

    Update prac-02.cpp

--- Expeiment 01/prac-02.cpp
+++ Expeiment 01/prac-02.cpp
@@ -1,6 +1,40 @@
 /* Write a C++ program to store roll no. (starting from 1), name and age of
 5 students and then print the details of the student with roll no. 2. */
 
+#include <iostream>
+using namespace std;
+struct student
+{
+   string name;
+   int age, roll;
+};
+int main()
+{
+   student stu[5];
+   int i;
+   cout << "Enter the information of all students: " << endl;
+   for (i = 1; i < 6; i++)
+   {
+      stu[i].roll = i;
+      cout << "For roll no: " << stu[i].roll << " : " << endl;
+      cout << "Enter name: ";
+      cin >> stu[i].name;
+      cout << "Enter age: ";
+      cin >> stu[i].age;
+      cout << endl;
+   }
+     cout << "Displaying Information of roll no 2: " << endl;
+      
+         cout<<"Name: "<<stu[2].name<<endl;
+         cout<<"Roll: "<<stu[2].roll<<endl;
+         cout<<"Age: "<<stu[2].age<<endl;
+      
+
+   return 0;
+}
+
+
+/*
 #include<iostream>
 using namespace std;
 struct student
@@ -43,3 +77,4 @@ int main(){
 
  return 0;
 }
+*/
